2c5af7a0b3f6f951f07cc13de85fc7c7338e35c7
Hecktox <max2003taube@gmail.com>
2023-11-09 13:32:52 -0500

    login gui

--- QuickStays/Views/Login/index.php
+++ QuickStays/Views/Login/index.php
@@ -10,25 +10,45 @@
 
 <head>
     <title>Login</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Include Bootstrap JavaScript and its dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.5/umd.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <section class="h-100 gradient-form" style="background-color: #00000;">
-        <h1>Login</h1>
+    <!-- Bootstrap Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">QuickStays</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Login <span class="sr-only">(current)</span></a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <h1 class="mb-3">Login</h1>
         <form method="POST" action="/eCommerce-Project/QuickStays/index.php?entity=login&action=login">
-            <label for="email">Email:</label>
-            <input type="email" name="email" required><br>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
 
-            <label for="password">Password:</label>
-            <input type="password" name="password" required><br>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" name="password" required>
+            </div>
 
-            <input type="submit" name="login" value="Login">
+            <button type="submit" class="btn btn-primary" name="login">Login</button>
         </form>
-        <button type="button"
+        <button type="button" class="btn btn-link"
             onclick="window.location.href='/eCommerce-Project/QuickStays/index.php?entity=user&action=register'">Not a
             Member? Register Here</button>
-        <button onclick="window.location.href='/eCommerce-Project/QuickStays'">Cancel</button>
-    </section>
+        <button class="btn btn-secondary" onclick="window.location.href='/eCommerce-Project/QuickStays'">Cancel</button>
+    </div>
 </body>
 
 </html>
