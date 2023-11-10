607dda3b026180e7d433b6f9d0e1a6704aa70614
Grimor1231 <132633625+Grimor1231@users.noreply.github.com>
2023-11-10 09:53:59 -0800

    Homepage update

--- QuickStays/Views/User/index.php
+++ QuickStays/Views/User/index.php
@@ -12,11 +12,8 @@
 
 <head>
     <title>Dashboard</title>
-    <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Include Bootstrap Optional theme -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap-theme.min.css">
-    <!-- Include Bootstrap JavaScript and its dependencies -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.9.5/umd.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -53,11 +50,9 @@
 
 <body>
     <?php
-    session_start(); // Start the session to work with $_SESSION variables
-    
+    session_start(); 
     // Check if the user is logged in
     if (isset($_SESSION['user_email'])) {
-        // Logged in user's email is in the session, display it
         echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">';
         echo '  <a class="navbar-brand" href="#">QuickStays</a>';
         echo '  <div class="collapse navbar-collapse" id="navbarNav">';
@@ -100,11 +95,10 @@
         </div>
     </div>
 
-    <!-- Add a section for featured spaces -->
+
     <div class="container">
         <h2>Find spaces that suit your style</h2>
         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <!-- Bootstrap carousel items -->
             <div class="carousel-inner">
                 <!-- First group of three items -->
                 <div class="carousel-item active">
@@ -132,8 +126,7 @@
                         </div>
                     </div>
                 </div>
-                <!-- Additional groups of three items as new .carousel-item elements -->
-                <!-- Example for the second group -->
+                <!-- 2nd group of items-->
                 <div class="carousel-item">
                     <div class="row">
                         <div class="col-md-4">
@@ -171,8 +164,93 @@
             </a>
         </div>
     </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="service-box">
 
-    <!-- End of featured spaces section -->
+                    <h3>Best Prices</h3>
+                    <p>Find the best deals for your stay.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+
+                    <h3>24/7 Support</h3>
+                    <p>Always there for you, any time, any place.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+
+                    <h3>Unique Experiences</h3>
+                    <p>Stay in unique homes across the world.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <h2 class="text-center mb-4">Happy Guests</h2>
+        <div class="row">
+            <!-- Testimonial 1 -->
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">"This was the perfect place for our weekend getaway. Quickstays made everything very easy for us!  "</p>
+                        <footer class="blockquote-footer">Jane Doe</footer>
+                    </div>
+                </div>
+            </div>
+            <!-- Testimonial 2 -->
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">"Amazing experience from start to finish. The booking process was easy and
+                            the stay exceeded our expectations."</p>
+                        <footer class="blockquote-footer">John Smith</footer>
+                    </div>
+                </div>
+            </div>
+            <!-- Testimonial 3 -->
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">"We loved the location and the amenities provided. It felt like a home away
+                            from home. We will definitely be back!"</p>
+                        <footer class="blockquote-footer">Emily Johnson</footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Newsletter Subscription Form -->
+    <div class="container mt-5">
+        <h2>Stay Updated</h2>
+        <form>
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="Enter your email">
+                <button type="submit" class="btn btn-primary">Subscribe</button>
+            </div>
+        </form>
+    </div>
+
+    <footer class="bg-light text-center text-lg-start mt-5">
+        <div class="container p-4">
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">QuickStays</h5>
+                    <p>
+                        Making every stay a memorable one.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
 
 </body>
 
