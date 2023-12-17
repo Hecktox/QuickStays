103a5a1ebc982e0aae97c4cfa89fd1f459ab0c2d
Grimor1231 <132633625+Grimor1231@users.noreply.github.com>
2023-12-16 20:31:25 -0500

    Implement rest of carousel

--- QuickStays/Views/User/index.php
+++ QuickStays/Views/User/index.php
@@ -194,182 +194,207 @@
                     </div>
                 </div>
 
-                <!-- 2nd group of items-->
+                <!-- 2nd group of items -->
                 <div class="carousel-item">
                     <div class="row">
+                        <!-- Cottage -->
                         <div class="col-md-4">
-                            <img src="/eCommerce-Project/QuickStays/images/cottage.jpg" class="d-block w-100"
-                                alt="Luxury House">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Cottage</h5>
-                            </div>
+                            <a
+                                href="/eCommerce-Project/QuickStays/index.php?entity=property&action=search&propertyType=House">
+                                <img src="/eCommerce-Project/QuickStays/images/cottage.jpg" class="d-block w-100"
+                                    alt="Cottage">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>House</h5>
+                                </div>
+                            </a>
                         </div>
+                        <!-- Penthouse -->
                         <div class="col-md-4">
-                            <img src="/eCommerce-Project/QuickStays/images/penthouse.jpeg" class="d-block w-100"
-                                alt="Luxury House">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Penthouse</h5>
-                            </div>
+                            <a
+                                href="/eCommerce-Project/QuickStays/index.php?entity=property&action=search&propertyType=Condo">
+                                <img src="/eCommerce-Project/QuickStays/images/penthouse.jpeg" class="d-block w-100"
+                                    alt="Penthouse">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Condo</h5>
+                                </div>
+                            </a>
                         </div>
+                        <!-- Luxury Apartment -->
                         <div class="col-md-4">
-                            <img src="/eCommerce-Project/QuickStays/images/apartment.webp" class="d-block w-100"
-                                alt="Luxury House">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Penthouse</h5>
-                            </div>
+                            <a
+                                href="/eCommerce-Project/QuickStays/index.php?entity=property&action=search&propertyType=Apartment">
+                                <img src="/eCommerce-Project/QuickStays/images/apartment.webp" class="d-block w-100"
+                                    alt="Luxury Apartment">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Luxury Apartment</h5>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- Carousel Controls -->
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
-    <!-- Services -->
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="service-box">
-                    <img src="/eCommerce-Project/QuickStays/images/dollar.png" alt="Best Prices"
-                        class="service-icon img-fluid">
-                    <h3>Best Prices</h3>
-                    <p>Find the best deals for your stay.</p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="service-box">
-                    <img src="/eCommerce-Project/QuickStays/images/support.png" alt="Best Prices"
-                        class="service-icon img-fluid">
-                    <h3>24/7 Support</h3>
-                    <p>Always there for you, any time, any place.</p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="service-box">
-                    <img src="/eCommerce-Project/QuickStays/images/happy.png" alt="Best Prices"
-                        class="service-icon img-fluid">
-                    <h3>Unique Experiences</h3>
-                    <p>Stay in unique homes across the world.</p>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <!-- Call to Action Section -->
-    <div class="container mt-4">
-        <div class="row">
-            <div class="col-12">
-                <div class="cta-section d-flex align-items-center justify-content-center"
-                    style="background: url('/eCommerce-Project/QuickStays/images/home.webp') no-repeat center center; background-size: cover; height: 400px;">
-                    <div class="text-center text-white" style="background: rgba(0, 0, 0, 0.5); padding: 20px;">
-                        <h2>List your property on QuickStays and open your door to rental income</h2>
-                        <p class="my-3">Earn money by renting out your vacation home, unused apartment, or extra room.
-                        </p>
-                        <a href="/eCommerce-Project/QuickStays/index.php?entity=user&action=register"
-                            class="btn btn-primary">Become A Host</a>
+                <!-- Carousel Controls -->
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <!-- Services -->
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="service-box">
+                        <img src="/eCommerce-Project/QuickStays/images/dollar.png" alt="Best Prices"
+                            class="service-icon img-fluid">
+                        <h3>Best Prices</h3>
+                        <p>Find the best deals for your stay.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-box">
+                        <img src="/eCommerce-Project/QuickStays/images/support.png" alt="Best Prices"
+                            class="service-icon img-fluid">
+                        <h3>24/7 Support</h3>
+                        <p>Always there for you, any time, any place.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-box">
+                        <img src="/eCommerce-Project/QuickStays/images/happy.png" alt="Best Prices"
+                            class="service-icon img-fluid">
+                        <h3>Unique Experiences</h3>
+                        <p>Stay in unique homes across the world.</p>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- Informational paragraphs -->
-        <div class="container  mt-4">
+        <!-- Call to Action Section -->
+        <div class="container mt-4">
             <div class="row">
                 <div class="col-12">
-                    <h2>Discover millions of rental options with QuickStays</h2>
-                    <p>For many travellers, a hotel just doesn’t cut it for a family trip or romantic couple’s retreat.
-                        Fortunately, QuickStays offers a diverse range of different vacation rentals that are suitable
-                        for all destinations and lifestyles, from idyllic cottages and quiet mountain chalets to
-                        waterfront homes and lakeside cabins. A world of possibilities awaits with a quick QuickStays
-                        property search, which shows you a list of millions of property rentals from private homeowners
-                        all over the world. Once you find your dream rental in your favourite destination, you can
-                        quickly and securely book the property to get your vacation planning started.</p>
-                    <h3>What can a vacation rental do for you?</h3>
-                    <p>Whether you’re embarking on a thrill-seeking excursion or looking for a quiet couple’s trip by
-                        the sea, vacation rentals are available in virtually every size and type. You can find cottages,
-                        cabins, lodges, houses, apartments, and more, all with a range of amenities to help you make the
-                        most of your trip. With a vacation rental, you can enjoy features like pet-friendly yards or
-                        gardens, pools and hot tubs, outdoor grilling, and entertainment space, all in the comfort of
-                        your own home-away-from-home. Best of all, you can find rentals for virtually any budget, with
-                        amenities that help you save, such as kitchens and entertainment features.</p>
+                    <div class="cta-section d-flex align-items-center justify-content-center"
+                        style="background: url('/eCommerce-Project/QuickStays/images/home.webp') no-repeat center center; background-size: cover; height: 400px;">
+                        <div class="text-center text-white" style="background: rgba(0, 0, 0, 0.5); padding: 20px;">
+                            <h2>List your property on QuickStays and open your door to rental income</h2>
+                            <p class="my-3">Earn money by renting out your vacation home, unused apartment, or extra
+                                room.
+                            </p>
+                            <a href="/eCommerce-Project/QuickStays/index.php?entity=user&action=register"
+                                class="btn btn-primary">Become A Host</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Informational paragraphs -->
+            <div class="container  mt-4">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Discover millions of rental options with QuickStays</h2>
+                        <p>For many travellers, a hotel just doesn’t cut it for a family trip or romantic couple’s
+                            retreat.
+                            Fortunately, QuickStays offers a diverse range of different vacation rentals that are
+                            suitable
+                            for all destinations and lifestyles, from idyllic cottages and quiet mountain chalets to
+                            waterfront homes and lakeside cabins. A world of possibilities awaits with a quick
+                            QuickStays
+                            property search, which shows you a list of millions of property rentals from private
+                            homeowners
+                            all over the world. Once you find your dream rental in your favourite destination, you can
+                            quickly and securely book the property to get your vacation planning started.</p>
+                        <h3>What can a vacation rental do for you?</h3>
+                        <p>Whether you’re embarking on a thrill-seeking excursion or looking for a quiet couple’s trip
+                            by
+                            the sea, vacation rentals are available in virtually every size and type. You can find
+                            cottages,
+                            cabins, lodges, houses, apartments, and more, all with a range of amenities to help you make
+                            the
+                            most of your trip. With a vacation rental, you can enjoy features like pet-friendly yards or
+                            gardens, pools and hot tubs, outdoor grilling, and entertainment space, all in the comfort
+                            of
+                            your own home-away-from-home. Best of all, you can find rentals for virtually any budget,
+                            with
+                            amenities that help you save, such as kitchens and entertainment features.</p>
+
+                    </div>
 
                 </div>
 
             </div>
 
-        </div>
-
-        <!-- Reviews/testimonials -->
-        <div class="container mt-5">
-            <h2 class="text-center mb-4">Happy Guests</h2>
-            <div class="row">
-                <!-- Testimonial 1 -->
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="card-text">"This was the perfect place for our weekend getaway. Quickstays made
-                                everything very easy for us! "</p>
-                            <footer class="blockquote-footer">John Snow</footer>
+            <!-- Reviews/testimonials -->
+            <div class="container mt-5">
+                <h2 class="text-center mb-4">Happy Guests</h2>
+                <div class="row">
+                    <!-- Testimonial 1 -->
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">"This was the perfect place for our weekend getaway. Quickstays
+                                    made
+                                    everything very easy for us! "</p>
+                                <footer class="blockquote-footer">John Snow</footer>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!-- Testimonial 2 -->
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="card-text">"Amazing experience from start to finish. The booking process was easy
-                                and
-                                the stay exceeded our expectations."</p>
-                            <footer class="blockquote-footer">Edward Scissorhands</footer>
+                    <!-- Testimonial 2 -->
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">"Amazing experience from start to finish. The booking process was
+                                    easy
+                                    and
+                                    the stay exceeded our expectations."</p>
+                                <footer class="blockquote-footer">Edward Scissorhands</footer>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!-- Testimonial 3 -->
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="card-text">"We loved the location and the amenities provided. It felt like a home
-                                away
-                                from home. We will definitely be back!"</p>
-                            <footer class="blockquote-footer">Emily Johnson</footer>
+                    <!-- Testimonial 3 -->
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">"We loved the location and the amenities provided. It felt like a
+                                    home
+                                    away
+                                    from home. We will definitely be back!"</p>
+                                <footer class="blockquote-footer">Emily Johnson</footer>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
-        <!-- Newsletter Subscription Form -->
-        <div class="container mt-5">
-            <h2>Stay Updated</h2>
-            <form>
-                <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Enter your email">
-                    <button type="submit" class="btn btn-primary">Subscribe</button>
-                </div>
-            </form>
-        </div>
+            <!-- Newsletter Subscription Form -->
+            <div class="container mt-5">
+                <h2>Stay Updated</h2>
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Enter your email">
+                        <button type="submit" class="btn btn-primary">Subscribe</button>
+                    </div>
+                </form>
+            </div>
 
-        <footer class="bg-light text-center text-lg-start mt-5">
-            <div class="container p-4">
+            <footer class="bg-light text-center text-lg-start mt-5">
+                <div class="container p-4">
 
-                <div class="row d-flex justify-content-center">
-                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">QuickStays</h5>
-                        <p>
-                            Making every stay a memorable one.
-                        </p>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">QuickStays</h5>
+                            <p>
+                                Making every stay a memorable one.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
 
 
 
